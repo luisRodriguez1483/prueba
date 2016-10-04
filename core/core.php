001726d6122ef0cf054808659dfcbb0762606333
luisRodriguez1483 <beto_61194@hotmail.com>
2016-10-04 12:04:27 -0500

    agregue constantes para la conexion en core.php

--- core/core.php
+++ core/core.php
@@ -4,4 +4,9 @@ Nucleo de la app y definicion de constantes
 */
 define ('HTML_DIR','html/');
 define ('APP_TITLE','SocialNetWork');
+
+define("HOST", 'localhost');
+define("USUARIO", 'root');
+define("PASSWORD", '');
+define("BASE",'socialNetWork');
 ?>
