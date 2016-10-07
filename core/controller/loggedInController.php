8041037a534b2d19d583af297eaf56ce6804da10
diego-nz <nunez.guzman@gmail.com>
2016-10-06 22:57:31 -0500

    ya solo falta jalar el data de respuesta en modelo ya no da el error del 500

--- core/controller/loggedInController.php
+++ core/controller/loggedInController.php
@@ -1,11 +1,10 @@
 <?php
-    if($_POST){
         if(isset($_POST["userName"]) && isset($_POST["pass"])){
+            echo getcwd();
             require_once 'core/model/userModel.php';
             $objUser=new ModelUser();
             $objUser->userLogin($_POST["userName"],$_POST["pass"]);
     }else{
             echo 'Error sin datos';
         }
-    }
 ?>
