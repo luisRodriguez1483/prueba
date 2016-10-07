5d9189b1c9ece745eaf454cc37a5571e54b761c4
diego-nz <nunez.guzman@gmail.com>
2016-10-07 00:17:14 -0500

    cambie por el nombre de las variables estaban al reves

--- core/controller/loggedInController.php
+++ core/controller/loggedInController.php
@@ -1,8 +1,8 @@
 <?php
 
     include '../model/ModelUser.php';
-    $password=$_POST["userName"];
-    $user=$_POST["pass"];
+    $user=$_POST["userName"];
+    $password=$_POST["pass"];
 
     $objUser = new ModelUser();
     $objUser->userLogin();
