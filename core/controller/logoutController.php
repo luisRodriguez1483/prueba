52763b2b4188b6987069f35bc81b4358598bfaa1
diego-nz <nunez.guzman@gmail.com>
2016-10-13 16:21:36 -0500

    validacion

--- core/controller/logoutController.php
+++ core/controller/logoutController.php
@@ -1,11 +1,14 @@
 <?php
-$logout=$_POST["action"];
-if($logout=="true"){
-    session_start();
-    session_unset();
-    session_destroy();
-    echo 1;
+if(isset($_POST["action"])){
+    $logout=$_POST["action"];
+    if($logout=="true"){
+        session_start();
+        session_unset();
+        session_destroy();
+        echo 1;
+    }
 }else{
     include ('view/index/index.php');
+    exit;
 }
 ?>
